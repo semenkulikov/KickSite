1dfcad646b9f7a990b55cfe6491cf7a4542310e8
Semen Saifutdinov <saifutdinov.semen@mail.ru>
2025-08-09 17:09:36 +0300

    Fixed command for export and import kick accounts 3

--- KickApp/management/commands/export_kickaccounts.py
+++ KickApp/management/commands/export_kickaccounts.py
@@ -26,6 +26,15 @@ class Command(BaseCommand):
             self.stdout.write(self.style.WARNING('Нет KickAccount для экспорта'))
             return
         
+        # Подсчитываем статистику
+        total_accounts = kick_accounts.count()
+        accounts_with_proxy = kick_accounts.filter(proxy__isnull=False).count()
+        accounts_without_proxy = kick_accounts.filter(proxy__isnull=True).count()
+        
+        self.stdout.write(f'Всего KickAccount: {total_accounts}')
+        self.stdout.write(f'С Proxy: {accounts_with_proxy}')
+        self.stdout.write(f'Без Proxy: {accounts_without_proxy}')
+        
         # Создаем CSV файл
         with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile)
@@ -45,7 +54,21 @@ class Command(BaseCommand):
             writer.writerow(headers)
             
             # Записываем данные
+            exported_with_proxy = 0
+            exported_without_proxy = 0
+            
             for account in kick_accounts:
+                # Получаем данные о Proxy
+                proxy_url = ''
+                proxy_status = ''
+                
+                if account.proxy:
+                    proxy_url = account.proxy.url
+                    proxy_status = str(account.proxy.status)
+                    exported_with_proxy += 1
+                else:
+                    exported_without_proxy += 1
+                
                 row = [
                     account.login,
                     account.token,
@@ -54,13 +77,15 @@ class Command(BaseCommand):
                     account.storage_state_path or '',
                     account.password or '',
                     account.storage_state_status or '',
-                    account.proxy.url if account.proxy else '',
-                    str(account.proxy.status) if account.proxy else ''
+                    proxy_url,
+                    proxy_status
                 ]
                 writer.writerow(row)
         
         self.stdout.write(
             self.style.SUCCESS(
-                f'Успешно экспортировано {kick_accounts.count()} KickAccount в {output_file}'
+                f'Успешно экспортировано {total_accounts} KickAccount в {output_file}\n'
+                f'  - С Proxy: {exported_with_proxy}\n'
+                f'  - Без Proxy: {exported_without_proxy}'
             )
         )
